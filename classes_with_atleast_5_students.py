8906618316f65799a65d57059a353f1060683133
rutvik <rutvikpatil3896@gmail.com>
2026-01-01 19:20:34 +0530

    added new file

---
+++ classes_with_atleast_5_students.py
@@ -0,0 +1,11 @@
+import pandas as pd
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    return (
+        courses
+        .groupby("class", as_index=False)
+        .size()
+        .query("size >= 5")
+        [["class"]]
+    )
+
+   
